d5c21c2feb01bd4507c0ff1ea2dba9491ebba82c
Gaurav Sarode <sgaurav@google.com>
2024-08-20 13:21:08 -0700

    Add husky_car target
    bug: TBD
    Test: NA
    Change-Id: I2ed6bf0521891ce51011204a0d5f077719dfcde6

---
+++ husky_car/aosp_husky_car.mk
@@ -0,0 +1,28 @@
+#
+# Copyright 2023 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_IS_64BIT_ONLY := true
+PIXEL_2023_GEN := true
+
+$(call inherit-product, device/google_car/common/pre_google_car.mk)
+$(call inherit-product, device/google_car/husky_car/device-husky-car.mk)
+$(call inherit-product, device/google_car/common/post_google_car.mk)
+
+PRODUCT_NAME := aosp_husky_car
+PRODUCT_DEVICE := husky
+PRODUCT_MODEL := AOSP on husky
+PRODUCT_BRAND := Android
+PRODUCT_MANUFACTURER := Google
